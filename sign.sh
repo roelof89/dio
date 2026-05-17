eb0746fce315313bbe61de877e43d449d0e53127
rudi <rudivw@intenda.za.com>
2026-05-17 16:25:09 +0200

    if I want to sign it

---
+++ sign.sh
@@ -0,0 +1,6 @@
+export APPLE_SIGNING_IDENTITY="Developer ID Application: Your Name (TEAM_ID)"
+export APPLE_ID="your@apple.id"
+export APPLE_PASSWORD="app-specific-password"
+export APPLE_TEAM_ID="YOUR_TEAM_ID"
+
+#security find-identity -v -p codesigning
